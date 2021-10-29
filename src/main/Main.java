8628dd0e9f8bc17be1e99d20082ab92dc24bc4c3
mehdimunim <68103264+mehdimunim@users.noreply.github.com>
2021-10-29 17:18:30 +0200

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,8 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("The game starts");
 		
-		System.out.println("");
-		
+		System.out.println("*".repeat(50));
+		for (int i=0; i<=50;i++) {
+			System.out.print("*" +" ".repeat(49) + "*\n");
+		}
 
 	}
 
